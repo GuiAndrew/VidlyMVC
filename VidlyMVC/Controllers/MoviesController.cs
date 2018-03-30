15b4fa60f0fda7057bfa639b3f395f320e1f7765
GuiAndrew <guiandrew.pt@gmail.com>
2018-03-30 14:12:02 +0100

    Method ActionResult Random

--- VidlyMVC/Controllers/MoviesController.cs
+++ VidlyMVC/Controllers/MoviesController.cs
@@ -10,11 +10,19 @@ namespace VidlyMVC.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ActionResult Random() //ActionResult is the base class for all action results in ASP.NET MVC.
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            //return View(movie); //We are calling this view method here which is just a helper method inherited from the base 
+            ////controller class.
+
+            ////Examples:
+            //return Content("Hello World!");
+            //return HttpNotFound(); //control shift B.
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" }); //First the action, next the controller, 
+            ////and the third is one anonyms method.
         }
     }
 }
