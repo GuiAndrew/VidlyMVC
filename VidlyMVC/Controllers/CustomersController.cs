deaee4f6ba3886cdf10c7b62322ed4d9c5a0f4dd
GuiAndrew <guiandrew.pt@gmail.com>
2018-04-10 12:04:22 +0100

    Querying Objects and Eager Loading

--- VidlyMVC/Controllers/CustomersController.cs
+++ VidlyMVC/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,18 +10,43 @@ namespace VidlyMVC.Controllers
 {
     public class CustomersController : Controller
     {
+        ///First we declare a field Db context to access the DB:
+        private ApplicationDbContext _context;
+
+        //We inialize the db context field in the ctor:
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //The db context is a dispose object, so we do a method to properly dispose it:
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-          
+            //var customers = GetCustomers(); //This call the method GetCustomers.
+
+            //var customers = _context.Customers; //This os to get the customers in database. But this is what we 
+            //call deferred execution. 
+
+            //var customers = _context.Customers.ToList(); //Wiht the ToList, will immediately call the customers.
+
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList(); //Eager Loading.
+
             return View(customers); //Will return the list of customers to the view Index.
         }
 
         // This method will pass the id of the single customer to the view to do the details of that customer.
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id); //This is to use the method GetCustomers().
+            //And to call the customer hard code in the nethod.
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id); //This call the customer in db.
 
             if (customer == null) //If don't have any customer, will return a error.
             {
@@ -30,15 +56,15 @@ namespace VidlyMVC.Controllers
             return View(customer);
         }
 
-        //This method is to do the list of customers.
-        private IEnumerable<Customer> GetCustomers()
-        {
-            var customers = new List<Customer> {
-                new Customer { Id = 1, Name = "Bruce Kim" },
-                new Customer { Id = 2, Name = "Blenda Kinda" }
-            };
+        ////This method is to do the list of customers.
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    var customers = new List<Customer> {
+        //        new Customer { Id = 1, Name = "Bruce Kim" },
+        //        new Customer { Id = 2, Name = "Blenda Kinda" }
+        //    };
 
-            return customers;
-        }
+        //    return customers;
+        //}
     }
 }
---
+++ VidlyMVC/README!!!!.txt
@@ -0,0 +1,41 @@
+﻿1 - We create some classes in Models folder to do the Customer, Movie and the MembershipType.
+
+2 - We create the controllers Customers and Movies. This is to create the object customer and the object movie.
+
+3 - In the controller Customers we create a method that create a "hardcoded" customers, this is only to see how this methods
+works and they pass information to the views.
+
+4 - We did this in the movies controller too.
+
+5 - And we create views to see this in action. But before we tested some methods without views.
+
+6 - We have create a folder ViewModels, with a class call RandomMovieViewModel.
+
+7 - We create migrations.
+
+8 - To enable migration, Command -> Enable-Migrations.
+
+9 - To add one migrations, Command -> Add-Migration NameOfThisMigration.
+
+10 - To update and execute the migration, Command -> Update-Migration.
+
+11 - If we want to recreate the migration we the same name was before, Command -> Add-Migration NameOfThisMigration -Force.
+(Will overwrite the last migration).
+
+12 - To seed the database, we create a empty migration and use the Sql method.
+
+13 - Migration aren't case sensitive.
+
+14 - In the view Index of Customers add a new <th> for discount rate and a new <td> to get the discount rate.
+
+15 - If we run the code now will give a NullReferenceException. So we have to related the customer and membershipType together, 
+this is call Eager Loading. In the Customerscontroller before the ToList() method, we have to add the Include method with the 
+Lampda, to call the MembershipType. In the top of the class put using System.Data.Entity. The full line:
+var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+16 - Now we will create a shortcut to package manager console. So we go to Tools -> Options -> In Environment, 
+select Keyboard -> In the TextBox Show commands containning, enter packagemanagerconsole, all together. -> In TextBox Press
+Shortcut Keys put the keys that will be the shortcut. In this case i chose alt + /. -> Next click Assing -> next Ok.
+NOTE: Be careful not to overlap with shorcut that are already created.
+
+17 - 
